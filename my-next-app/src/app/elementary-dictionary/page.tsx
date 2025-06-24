f0e16c415a353162a3c63faf654df4b5ac048a4e
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-06-24 19:38:58 +0300

    добавлена страница словаря

---
+++ my-next-app/src/app/elementary-dictionary/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ElementaryDictionary from '@/pages/ElementaryDictionary/ElementaryDictionary';
+
+const ElementaryDictionaryPage = () => <ElementaryDictionary />;
+
+export default ElementaryDictionaryPage; 
