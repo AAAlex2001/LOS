e2a5e50ebd0306666dae4c044b722e16f38fe5d2
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-06-29 16:58:41 +0300

    город сухум

---
+++ my-next-app/src/app/cities/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import Cities from '@/pages/Cities/Cities';
+
+const CitiesPage: React.FC = () => {
+  return <Cities />;
+};
+
+export default CitiesPage; 
---
+++ my-next-app/src/pages/Cities/Cities.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import ScrollToTop from '@/components/ScrollToTop/ScrollToTop';
+import CitiesSukhum from './CitiesSukhum';
+import styles from './Cities.module.scss';
+
+const Cities: React.FC = () => {
+  return (
+    <div className={styles.pageWrapper}>
+      <Header />
+
+      <main className={styles.mainContent}>
+        <h1 className={styles.mainTitle}>Города Абхазии</h1>
+
+        {/* Контент города Сухум */}
+        <CitiesSukhum />
+      </main>
+
+      <Footer />
+
+      {/* Кнопка прокрутки вверх */}
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default Cities; 
