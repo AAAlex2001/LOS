c287c0892b74a40ad7a82ed1df4ce5ae86cdffaf
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-06-29 19:55:37 +0300

    сухум винодельни

---
+++ my-next-app/src/app/wineries/Sukhum/page.tsx
@@ -0,0 +1,5 @@
+import WineriesSukhum from '@/pages/Cities/wineries/Sukhum/WineriesSukhum';
+
+export default function Page() {
+  return <WineriesSukhum />;
+} 
--- my-next-app/src/pages/Cities/CitiesSukhum.tsx
+++ my-next-app/src/pages/Cities/CitiesSukhum.tsx
@@ -43,6 +43,9 @@ const CitiesSukhum: React.FC = () => {
       case 'Аптеки':
         router.push('/pharmacy/Sukhum');
         break;
+      case 'Винодельни':
+        router.push('/wineries/Sukhum');
+        break;
       // Добавить другие категории по мере необходимости
       default:
         break;
@@ -80,7 +83,7 @@ const CitiesSukhum: React.FC = () => {
             {highlightItems.map((item) => (
               <li 
                 key={item} 
-                className={`${styles.listItem} ${(item === 'Административные здания' || item === 'Аптеки') ? styles.clickableItem : ''}`}
+                className={`${styles.listItem} ${(item === 'Административные здания' || item === 'Аптеки' || item === 'Винодельни') ? styles.clickableItem : ''}`}
                 onClick={() => handleItemClick(item)}
               >
                 <span className={styles.arrowIcon} />
