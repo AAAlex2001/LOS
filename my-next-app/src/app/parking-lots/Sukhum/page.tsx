9a178f4f11591016926a18d57b7384ba16845034
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-06-29 21:37:59 +0300

    сухум парковки

---
+++ my-next-app/src/app/parking-lots/Sukhum/page.tsx
@@ -0,0 +1,5 @@
+import ParkingLotsSukhum from '@/pages/Cities/parking-lots/Sukhum/ParkingLotsSukhum';
+
+export default function Page() {
+  return <ParkingLotsSukhum />;
+} 
--- my-next-app/src/pages/Cities/CitiesSukhum.tsx
+++ my-next-app/src/pages/Cities/CitiesSukhum.tsx
@@ -61,6 +61,9 @@ const CitiesSukhum: React.FC = () => {
       case 'Отели':
         router.push('/hotels/Sukhum');
         break;
+      case 'Парковки':
+        router.push('/parking-lots/Sukhum');
+        break;
       // Добавить другие категории по мере необходимости
       default:
         break;
@@ -98,7 +101,7 @@ const CitiesSukhum: React.FC = () => {
             {highlightItems.map((item) => (
               <li 
                 key={item} 
-                className={`${styles.listItem} ${(item === 'Административные здания' || item === 'Аптеки' || item === 'Винодельни' || item === 'Заправки' || item === 'Культурные достопримечательности' || item === 'Мойки' || item === 'Магазины и рынки' || item === 'Отели') ? styles.clickableItem : ''}`}
+                className={`${styles.listItem} ${(item === 'Административные здания' || item === 'Аптеки' || item === 'Винодельни' || item === 'Заправки' || item === 'Культурные достопримечательности' || item === 'Мойки' || item === 'Магазины и рынки' || item === 'Отели' || item === 'Парковки') ? styles.clickableItem : ''}`}
                 onClick={() => handleItemClick(item)}
               >
                 <span className={styles.arrowIcon} />
