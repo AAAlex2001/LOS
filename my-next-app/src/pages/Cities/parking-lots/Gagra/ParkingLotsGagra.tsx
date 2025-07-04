73606c0ac43eb881551e571b8e1779366813047f
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-07-04 18:02:01 +0300

    парковки

--- my-next-app/src/pages/Cities/parking-lots/Gagra/ParkingLotsGagra.tsx
+++ my-next-app/src/pages/Cities/parking-lots/Gagra/ParkingLotsGagra.tsx
@@ -5,62 +5,38 @@ import Image from 'next/image';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import ScrollToTop from '@/components/ScrollToTop/ScrollToTop';
-import styles from './ParkingLotsSukhum.module.scss';
+import styles from './ParkingLotsGagra.module.scss';
 
 // Данные парковок
 const parkingLots = [
   {
     id: 1,
-    name: 'Парковка',
-    address: 'г. Сухум , ул.Эшба 166',
-    addressLink: 'https://www.google.com/maps/place/166+Eshba+St,+Sochumi/data=!4m2!3m1!1s0x405f2269189966f9:0xbfb190c2cf3f48ec?sa=X&ved=1t:242&ictx=111',
-    image: '/assets/ParkingLotsSukhum1.jpg',
+    name: 'Автостоянка Гагра 1',
+    address: 'ул.Рашида Гечба 40',
+    addressLink: null,
+    contacts: '+79409932322',
+    workingHours: 'круглосуточно',
+    image: '/assets/city_gagra.jpg',
   },
   {
     id: 2,
-    name: 'Парковка',
-    address: 'г. Сухум , Кодорское шоссе 665',
-    addressLink: 'https://www.google.com/maps/place/665+Kodori+Hwy,+Sokhumi/data=!4m2!3m1!1s0x405f203330612ff3:0xdd80f796f258113c?sa=X&ved=1t:242&ictx=111',
-    image: '/assets/ParkingLotsSukhum2.jpg',
-  },
-  {
-    id: 3,
-    name: 'Парковка у пляжа Мокко',
-    address: 'г. Сухум Мокко Пляж',
-    addressLink: 'https://maps.app.goo.gl/iv7Kxunm2NvTb9vv9',
-    image: '/assets/ParkingLotsSukhum3.jpg',
-  },
-  {
-    id: 4,
-    name: 'Парковка',
-    address: 'г. Сухум, Кодорское шоссе',
-    addressLink: 'https://maps.app.goo.gl/DKPBtynWm4qu1ZhLA',
-    image: '/assets/ParkingLotsSukhum4.jpg',
-  },
-  {
-    id: 5,
-    name: 'Парковка',
-    address: 'г. Сухум, Кодорское шоссе 57',
-    addressLink: 'https://maps.app.goo.gl/3NKxiQwMZiA34Fd89',
-    image: '/assets/ParkingLotsSukhum5.jpg',
-  },
-  {
-    id: 6,
-    name: 'Парковка',
-    address: 'г. Сухум, ул. Аидгылара',
-    addressLink: 'https://maps.app.goo.gl/EtaMaKjAzH4ENMh1A',
-    image: '/assets/ParkingLotsSukhum6.jpg',
+    name: 'Автостоянка Гагра 2',
+    address: 'ул.Рашида Гечба 40',
+    addressLink: null,
+    contacts: '+79409932322',
+    workingHours: 'круглосуточно',
+    image: '/assets/city_gagra.jpg',
   },
 ];
 
-const ParkingLotsSukhum: React.FC = () => {
+const ParkingLotsGagra: React.FC = () => {
   return (
     <div className={styles.pageWrapper}>
       <Header />
       
       <main className={styles.mainContent}>
         <section className={styles.titleSection}>
-          <h1 className={styles.mainTitle}>Сухум: парковки для автомобилей</h1>
+          <h1 className={styles.mainTitle}>Гагра: парковки для автомобилей</h1>
         </section>
 
         <section className={styles.cardsSection}>
@@ -89,6 +65,20 @@ const ParkingLotsSukhum: React.FC = () => {
                       )}
                     </span>
                   </div>
+
+                  {lot.contacts && (
+                    <div className={styles.infoItem}>
+                      <span className={styles.infoLabel}>Контакты:</span>
+                      <span className={styles.infoValue}>{lot.contacts}</span>
+                    </div>
+                  )}
+
+                  {lot.workingHours && (
+                    <div className={styles.infoItem}>
+                      <span className={styles.infoLabel}>Часы работы:</span>
+                      <span className={styles.infoValue}>{lot.workingHours}</span>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -102,4 +92,4 @@ const ParkingLotsSukhum: React.FC = () => {
   );
 };
 
-export default ParkingLotsSukhum; 
+export default ParkingLotsGagra; 
--- my-next-app/src/pages/Cities/parking-lots/NewAfon/ParkingLotsNewAfon.tsx
+++ my-next-app/src/pages/Cities/parking-lots/NewAfon/ParkingLotsNewAfon.tsx
@@ -5,62 +5,30 @@ import Image from 'next/image';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import ScrollToTop from '@/components/ScrollToTop/ScrollToTop';
-import styles from './ParkingLotsSukhum.module.scss';
+import styles from './ParkingLotsNewAfon.module.scss';
 
 // Данные парковок
 const parkingLots = [
   {
     id: 1,
-    name: 'Парковка',
-    address: 'г. Сухум , ул.Эшба 166',
-    addressLink: 'https://www.google.com/maps/place/166+Eshba+St,+Sochumi/data=!4m2!3m1!1s0x405f2269189966f9:0xbfb190c2cf3f48ec?sa=X&ved=1t:242&ictx=111',
-    image: '/assets/ParkingLotsSukhum1.jpg',
-  },
-  {
-    id: 2,
-    name: 'Парковка',
-    address: 'г. Сухум , Кодорское шоссе 665',
-    addressLink: 'https://www.google.com/maps/place/665+Kodori+Hwy,+Sokhumi/data=!4m2!3m1!1s0x405f203330612ff3:0xdd80f796f258113c?sa=X&ved=1t:242&ictx=111',
-    image: '/assets/ParkingLotsSukhum2.jpg',
-  },
-  {
-    id: 3,
-    name: 'Парковка у пляжа Мокко',
-    address: 'г. Сухум Мокко Пляж',
-    addressLink: 'https://maps.app.goo.gl/iv7Kxunm2NvTb9vv9',
-    image: '/assets/ParkingLotsSukhum3.jpg',
-  },
-  {
-    id: 4,
-    name: 'Парковка',
-    address: 'г. Сухум, Кодорское шоссе',
-    addressLink: 'https://maps.app.goo.gl/DKPBtynWm4qu1ZhLA',
-    image: '/assets/ParkingLotsSukhum4.jpg',
-  },
-  {
-    id: 5,
-    name: 'Парковка',
-    address: 'г. Сухум, Кодорское шоссе 57',
-    addressLink: 'https://maps.app.goo.gl/3NKxiQwMZiA34Fd89',
-    image: '/assets/ParkingLotsSukhum5.jpg',
-  },
-  {
-    id: 6,
-    name: 'Парковка',
-    address: 'г. Сухум, ул. Аидгылара',
-    addressLink: 'https://maps.app.goo.gl/EtaMaKjAzH4ENMh1A',
-    image: '/assets/ParkingLotsSukhum6.jpg',
+    name: 'Автомобильная парковка',
+    address: 'Гудаутский район, Новый Афон , ул. Кяхба',
+    addressLink: null,
+    contacts: '+79407507770',
+    workingHours: 'с 08:00 до 21:00',
+    price: '150руб.',
+    image: '/assets/city_newafon.jpg',
   },
 ];
 
-const ParkingLotsSukhum: React.FC = () => {
+const ParkingLotsNewAfon: React.FC = () => {
   return (
     <div className={styles.pageWrapper}>
       <Header />
       
       <main className={styles.mainContent}>
         <section className={styles.titleSection}>
-          <h1 className={styles.mainTitle}>Сухум: парковки для автомобилей</h1>
+          <h1 className={styles.mainTitle}>Новый Афон: парковки для автомобилей</h1>
         </section>
 
         <section className={styles.cardsSection}>
@@ -89,6 +57,27 @@ const ParkingLotsSukhum: React.FC = () => {
                       )}
                     </span>
                   </div>
+
+                  {lot.contacts && (
+                    <div className={styles.infoItem}>
+                      <span className={styles.infoLabel}>Контакты:</span>
+                      <span className={styles.infoValue}>{lot.contacts}</span>
+                    </div>
+                  )}
+
+                  {lot.workingHours && (
+                    <div className={styles.infoItem}>
+                      <span className={styles.infoLabel}>Часы работы:</span>
+                      <span className={styles.infoValue}>{lot.workingHours}</span>
+                    </div>
+                  )}
+
+                  {lot.price && (
+                    <div className={styles.infoItem}>
+                      <span className={styles.infoLabel}>Цена:</span>
+                      <span className={styles.infoValue}>{lot.price}</span>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -102,4 +91,4 @@ const ParkingLotsSukhum: React.FC = () => {
   );
 };
 
-export default ParkingLotsSukhum; 
+export default ParkingLotsNewAfon; 
