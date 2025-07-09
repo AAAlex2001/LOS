04b133a5b5e4fe0695f1fda00dad3b3645576699
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-07-09 20:20:41 +0300

    правки банки  + ссылки на главной странице

--- my-next-app/src/components/Popup/Popup.tsx
+++ my-next-app/src/components/Popup/Popup.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 import styles from './Popup.module.scss';
 
-interface PopupItem {
-  label: string;
-  href: string;
-}
+const popupItems = [
+  { label: 'Города Абхазии', href: '/cities' },
+  // { label: 'Аренда жилья', href: '/rent' },
+  { label: 'Связь', href: '/mobile-communication' },
+  { label: 'Такси', href: '/taxi' },
+  { label: 'Банки', href: '/banks' },
+];
 
-interface PopupProps {
-  items: PopupItem[];
-}
-
-const Popup: React.FC<PopupProps> = ({ items }) => {
+const Popup: React.FC = () => {
   return (
     <div className={styles.popup}>
       <div className={styles.popupVariaties}>
-        {items.map((item, index) => (
+        {popupItems.map((item, index) => (
           <Link key={index} href={item.href} className={styles.inactive}>
             <span className={styles.popupText}>{item.label}</span>
           </Link>
