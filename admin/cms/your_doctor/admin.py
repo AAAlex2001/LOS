ff6d14dccb1a6c4b0c9de4967ce5967ede2769ba
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-09-21 20:06:22 +0300

    правки бекенд

--- admin/cms/your_doctor/admin.py
+++ admin/cms/your_doctor/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import (
     YourDoctorPage,
@@ -13,29 +14,57 @@ from .models import (
 class HospitalInline(admin.TabularInline):
     model = Hospital
     extra = 0
-    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "order")
+    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "image_preview", "order")
+    readonly_fields = ("image_preview",)
     ordering = ("order", "id")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 100px;" />', obj.image.url)
+        return "Изображение не загружено"
+    image_preview.short_description = "Превью"
 
 
 class PrivateClinicInline(admin.TabularInline):
     model = PrivateClinic
     extra = 0
-    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "order")
+    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "image_preview", "order")
+    readonly_fields = ("image_preview",)
     ordering = ("order", "id")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 100px;" />', obj.image.url)
+        return "Изображение не загружено"
+    image_preview.short_description = "Превью"
 
 
 class DentistryInline(admin.TabularInline):
     model = Dentistry
     extra = 0
-    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "order")
+    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "image_preview", "order")
+    readonly_fields = ("image_preview",)
     ordering = ("order", "id")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 100px;" />', obj.image.url)
+        return "Изображение не загружено"
+    image_preview.short_description = "Превью"
 
 
 class VetClinicInline(admin.TabularInline):
     model = VetClinic
     extra = 0
-    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "order")
+    fields = ("name", "name_link", "working_hours", "address", "address_link", "contacts", "image", "image_preview", "order")
+    readonly_fields = ("image_preview",)
     ordering = ("order", "id")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 100px;" />', obj.image.url)
+        return "Изображение не загружено"
+    image_preview.short_description = "Превью"
 
 
 class DoctorsGroupInline(admin.TabularInline):
@@ -49,6 +78,52 @@ class DoctorsGroupInline(admin.TabularInline):
 class YourDoctorPageAdmin(admin.ModelAdmin):
     list_display = ("id", "main_title", "created_at", "updated_at")
     inlines = [HospitalInline, PrivateClinicInline, DentistryInline, VetClinicInline, DoctorsGroupInline]
+    readonly_fields = ("logo_preview", "hospitals_hero_preview", "og_image_preview", "twitter_image_preview")
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("main_title", "logo_image", "logo_preview", "hospitals_hero_image", "hospitals_hero_preview")
+        }),
+        ("SEO", {
+            "fields": ("seo_title", "seo_description", "seo_keywords", "canonical_url"),
+            "classes": ("collapse",)
+        }),
+        ("Open Graph", {
+            "fields": ("og_title", "og_description", "og_image", "og_image_preview"),
+            "classes": ("collapse",)
+        }),
+        ("Twitter Cards", {
+            "fields": ("twitter_title", "twitter_description", "twitter_image", "twitter_image_preview"),
+            "classes": ("collapse",)
+        }),
+        ("Robots", {
+            "fields": ("robots_index", "robots_follow"),
+            "classes": ("collapse",)
+        }),
+    )
+
+    def logo_preview(self, obj):
+        if obj.logo_image:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 200px;" />', obj.logo_image.url)
+        return "Изображение не загружено"
+    logo_preview.short_description = "Превью логотипа"
+
+    def hospitals_hero_preview(self, obj):
+        if obj.hospitals_hero_image:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 200px;" />', obj.hospitals_hero_image.url)
+        return "Изображение не загружено"
+    hospitals_hero_preview.short_description = "Превью изображения блока 'Больницы'"
+
+    def og_image_preview(self, obj):
+        if obj.og_image:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 200px;" />', obj.og_image.url)
+        return "Изображение не загружено"
+    og_image_preview.short_description = "Превью Open Graph изображения"
+
+    def twitter_image_preview(self, obj):
+        if obj.twitter_image:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 200px;" />', obj.twitter_image.url)
+        return "Изображение не загружено"
+    twitter_image_preview.short_description = "Превью Twitter Card изображения"
 
     def has_add_permission(self, request):
         # Разрешаем только одну страницу "Ваш доктор"
