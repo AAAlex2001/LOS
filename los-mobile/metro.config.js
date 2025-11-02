51bde8759e20f5220acae17b33e39c5049554181
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-11-02 12:30:48 +0300

    ком

--- los-mobile/metro.config.js
+++ los-mobile/metro.config.js
@@ -1,10 +1,3 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-// Enable importing SVGs as React components
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
-config.resolver.sourceExts.push('svg');
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
