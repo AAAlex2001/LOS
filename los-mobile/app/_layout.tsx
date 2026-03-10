5a96d1123ac4e6a9f2ac5efcc7c1b9c7e725a04f
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-03-10 10:25:17 +0300

    1

--- los-mobile/app/_layout.tsx
+++ los-mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
 import * as SystemUI from 'expo-system-ui';
 import 'react-native-reanimated';
@@ -11,18 +12,14 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
     SystemUI.setBackgroundColorAsync('#010E59');
+    SplashScreen.hideAsync().catch(() => {});
   }, []);
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
